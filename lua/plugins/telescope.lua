c4365117817d4818d9033c3e2850e4e9f9436cef
Ladas552 <l.tokshalov@gmail.com>
2025-02-27 08:36:56 +0500

    Telescope diagnostics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "find files" })
+vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files" })
 
-
-vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent files" })
+vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Recent Files" })
 
 vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Rip-grep" })
 
-vim.keymap.set("n", "<leader>fc", "<cmd>Telescope find_files<cr>", { desc = "Grep curent buffer" })
+vim.keymap.set("n", "<leader>fc", "<cmd>Telescope find_files<cr>", { desc = "Grep Curent Buffer" })
 
 vim.keymap.set("n", "<leader>fn", "<cmd>Telescope neorg find_norg_files<cr>", { desc = "Find Norg File" })
 
 vim.keymap.set("n", "<leader>n", "<cmd>Telescope neorg switch_workspace<cr>", { desc = "Change Neorg Workspace" })
+
+vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<cr>", { desc = "Find Diagnostics" })
